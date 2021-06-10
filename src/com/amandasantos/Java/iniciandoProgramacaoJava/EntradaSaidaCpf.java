5f94c2a3d39e084d48bee058cfd4d56792b6ecf1
Amanda Santos <amndmsilva@gmail.com>
2021-06-09 22:56:51 -0300

    adicionando descrições dos desafios

--- src/com/amandasantos/Java/iniciandoProgramacaoJava/EntradaSaidaCpf.java
+++ src/com/amandasantos/Java/iniciandoProgramacaoJava/EntradaSaidaCpf.java
@@ -1,3 +1,18 @@
+/* DESAFIO
+  Elabore um programa que possuas as características abaixo:
+     1 - Leia os dados de um CPF no formato XXX.YYY.ZZZ-DD;
+     2- Imprima os quatro grupos numéricos, sendo um valor por linha.
+
+ENTRADA
+  A entrada consiste vários arquivos de teste e cada um possuindo uma linha com formato XXX.YYY.ZZZ-DD,
+  onde XXX, YYY, ZZZ, DD são números inteiros.
+
+SAÍDA
+  Para cada arquivo da entrada, tem que ter um arquivo de saída com quatro linhas, e em cada linha um
+  número inteiro de acordo com procedimento 2 descrito no Desafio.
+
+*/
+
 package com.amandasantos.Java.iniciandoProgramacaoJava;
 
 import java.util.Scanner;
--- src/com/amandasantos/Java/iniciandoProgramacaoJava/ExibindoNumerosPares.java
+++ src/com/amandasantos/Java/iniciandoProgramacaoJava/ExibindoNumerosPares.java
@@ -1,3 +1,14 @@
+/* DESAFIO
+    Crie um programa que leia um número e mostre os números pares até esse número, inclusive ele mesmo.
+
+ENTRADA
+    Você receberá 1 valor inteiro N, onde N > 0.
+
+SAÍDA
+    Exiba todos os números pares até o valor de entrada, sendo um em cada linha.
+
+*/
+
 package com.amandasantos.Java.iniciandoProgramacaoJava;
 
 import java.io.BufferedReader;
